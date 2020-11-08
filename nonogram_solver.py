b5bda48a7b6af083d85877d091407967c231dbda
Adam Young <theyoungadam@gmail.com>
2020-11-08 00:50:19 -0700

    Added nonogram_counter_space function

--- nonogram_solver.py
+++ nonogram_solver.py
@@ -37,8 +37,22 @@ def read_nonogram(filename):
 
 
 def generate_nonogram_grid():
-    nonogram = np.zeros((5, 5), dtype=str)
-    print(nonogram)
+    nonogram = np.zeros((5, 5), dtype=int)
+    return(nonogram)
+
+
+def nonogram_counter_space(col_counters, row_counters, nonogram):
+    for i in range(5):
+        if 5 - (sum(col_counters[i]) + len(col_counters[i]) - 1) == 0:
+            index = 0
+            for counter in col_counters[i]:
+                for n in range(counter):
+                    nonogram[index, i] = 1
+                    index += 1
+                if index < 4:
+                    nonogram[index, i] = 2
+                    index += 1
+    return(nonogram)
 
 
 if __name__ == "__main__":
@@ -47,5 +61,6 @@ if __name__ == "__main__":
     # col_counters counts the number of activated pixels in each column of the
     # grid, and row_counters does the same for the rows. The nonogram will be
     # solved from this input data.
-    generate_nonogram_grid()
-
+    nonogram = generate_nonogram_grid()
+    nonogram_counter_space(col_counters, row_counters, nonogram)
+    print(nonogram)
