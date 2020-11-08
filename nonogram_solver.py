0c66f6e99c6a2be31dedba629eb4e0f318b51884
Adam Young <theyoungadam@gmail.com>
2020-11-07 23:29:22 -0700

    Adding key files to the repo

---
+++ nonogram_solver.py
@@ -0,0 +1,51 @@
+# nonogram_solver.py
+import sys
+import numpy as np
+
+
+def read_command_line():
+    """
+    Reads command line for additional argument that specifies file to be read.
+    "python3 nonogram_solver.py <filename.txt>"
+    If no filename is specified the default filename "gram1.txt" is used instead.
+    sys.argv[1] = "<filename.txt>" in the case of the example above.
+    """
+    try:
+        return(sys.argv[1])
+    except:
+        return("gram1.txt")
+
+
+def read_nonogram(filename):
+    """
+    From the selected filename (see read_command_line) this reads the input,
+    and formats it into a list of lists for both the x and y counters.
+    The counters are the numbers that indicate the number of activated pixels
+    in each row (y) and each column (x).
+    """
+    file = open(filename)
+    (x0, y0) = file.read().split('\n')
+    x1 = x0.split(',')
+    y1 = y0.split(',')
+    xf = []
+    yf = []
+    for i in range(len(x1)):
+        xf += [[int(j) for j in x1[i].split()]]
+    for i in range(len(y1)):
+        yf += [[int(j) for j in y1[i].split()]]
+    return(xf, yf)
+
+
+def generate_nonogram_grid():
+    nonogram = np.zeros((5, 5), dtype=str)
+    print(nonogram)
+
+
+if __name__ == "__main__":
+    filename = read_command_line()
+    (col_counters, row_counters) = read_nonogram(filename)
+    # col_counters counts the number of activated pixels in each column of the
+    # grid, and row_counters does the same for the rows. The nonogram will be
+    # solved from this input data.
+    generate_nonogram_grid()
+
